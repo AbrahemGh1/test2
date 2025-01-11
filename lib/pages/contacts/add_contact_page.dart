bcef8df870c97355aaa22726028fe5c68873571c
AbrahemGh1 <abrahem_se@yahoo.com>
2025-01-11 03:59:10 +0300

    add section option to add form.

--- lib/pages/contacts/add_contact_page.dart
+++ lib/pages/contacts/add_contact_page.dart
@@ -7,11 +7,36 @@ import 'package:flareline_uikit/components/modal/modal_dialog.dart';
 import 'package:flareline_uikit/components/tables/table_widget.dart';
 import 'package:flareline_uikit/core/mvvm/base_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:group_button/group_button.dart';
 
 import 'date_picker_widget.dart';
 
+class ScrollInjector extends StatelessWidget {
+  const ScrollInjector({
+    Key? key,
+    required this.child,
+    required this.groupingType,
+  }) : super(key: key);
+
+  final Widget child;
+  final GroupingType groupingType;
+
+  @override
+  Widget build(BuildContext context) {
+    if (groupingType == GroupingType.row) {
+      return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: child,
+      );
+    }
+    return child;
+  }
+}
+
 class AddContactPage extends BaseWidget<AddContactViewModel> {
   AddContactPage({super.key});
+
+  final controller = GroupButtonController();
 
   @override
   Widget bodyWidget(
@@ -56,15 +81,41 @@ class AddContactPage extends BaseWidget<AddContactViewModel> {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       DatePickerWidget(),
-                      const SizedBox(
-                        height: 16,
-                      ),
                     ]),
-                const SizedBox(
-                  height: 12,
-                ),
-                const SizedBox(
-                  height: 12,
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: Row(
+                    children: [
+                      const Text(
+                        'القسم',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(width: 20),
+                      ScrollInjector(
+                        groupingType: GroupingType.wrap,
+                        child: GroupButton(
+                          buttons: const [
+                            'رجال',
+                            'نساء',
+                            'اطفال',
+                          ],
+                          controller: GroupButtonController(selectedIndex: 0),
+                          options: GroupButtonOptions(
+                            selectedShadow: const [],
+                            unselectedShadow: const [],
+                            unselectedColor: Colors.grey[300],
+                            unselectedTextStyle: TextStyle(
+                              color: Colors.grey[600],
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          onSelected: (val, i, selected) =>
+                              debugPrint('Button: $val index: $i $selected'),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
                 const Divider(),
                 const SizedBox(
@@ -105,12 +156,12 @@ class AddContactPage extends BaseWidget<AddContactViewModel> {
         hintText: hint,
         hintStyle: const TextStyle(fontSize: 14, color: CrmColors.paragraph),
         icon: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 5),
           child: ImageWidget(
             imageUrl: 'assets/crm/$svg',
             width: 20,
             height: 20,
           ),
-          margin: EdgeInsets.symmetric(horizontal: 5),
         ));
   }
 
